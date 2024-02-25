0a962b472fbee2d6a76cd78f0f38bbc350eac9aa
artur.yumaev <artur.yumaev@sbermarket.ru>
2024-02-25 23:54:12 +0300

    Added commands for docker registry

--- scripts/api.mk
+++ scripts/api.mk
@@ -3,6 +3,7 @@ NAME?=api
 VERSION?=0.0.1
 COMMIT?=$(shell git rev-parse --short HEAD)
 BUILD_TIME?=$(shell date '+%Y-%m-%dT%H:%M:%S')
+CONTAINER_NAME?=docker.io/arturyumaev/${NAME}
 
 clean:
 	rm bin/api
@@ -16,14 +17,18 @@ api_build: clean
 		-X ${PROJECT}/version.BuildTime=${BUILD_TIME}" \
 		-o bin/api cmd/api/main.go 
 
-api_build_docker:
-	docker build -t ${NAME}:${VERSION} -t ${NAME}:latest -f deployments/production/Dockerfile.api .
-
 api_start: api_build
 	./bin/api
 
-api_start_docker: api_build_docker
-	docker run --rm -p 3000:3000 --env-file deployments/production/.env ${NAME}:${VERSION}
+api_docker_build:
+	docker build -t ${CONTAINER_NAME}:${VERSION} -t ${CONTAINER_NAME}:latest -f deployments/production/Dockerfile.api .
+
+api_docker_start: api_docker_build
+	docker run --rm -p 3000:3000 --env-file deployments/production/.env ${CONTAINER_NAME}:${VERSION}
+
+api_docker_push:
+	docker push ${CONTAINER_NAME}:${VERSION}
+	docker push ${CONTAINER_NAME}:latest
 
 api_status:
 	curl localhost:3000/status | jq
